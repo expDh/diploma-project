b5b9a3f4c02633566c243d7e00ad06b97ddbe103
Kirill <kirya_gla@mail.ru>
2026-06-10 03:04:18 +0300

    first commit

--- diplom-frontend1/src/api/axiosInstance.ts
+++
@@ -1,9 +0,0 @@
-// src/api/axiosInstance.js
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4200', // адрес твоего бекенда
-  withCredentials: true, // обязательно, чтобы браузер отправлял куки
-});
-
-export default axiosInstance;
