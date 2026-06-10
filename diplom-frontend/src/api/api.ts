ce2b6ab58f069ac780cb8441ecf07a0ea08a2344
Kirill <kirya_gla@mail.ru>
2026-06-10 23:22:56 +0300

    url

--- diplom-frontend/src/api/api.ts
+++ diplom-frontend/src/api/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4200',
+  // baseURL: 'http://localhost:4200',
+  baseURL: 'http://217.198.6.94',
+
   withCredentials: true,
 });
 
